3f872b621168e3b9a261af835e431cb05d16711f
The Fledgling Fleet <thefledglingfleet@gmail.com>
2020-11-20 21:59:16 -0600

    newMain is main, and Tessle is added
    Added newMain, to the boot script, which now uses the robot script. Also, adds my test bot Tessle to the configuration. Hence why robot.py is added.

--- newMain.py
+++ newMain.py
@@ -1,17 +1,15 @@
-# New Main #
-import pyfirmata
 from inputs import get_gamepad
 from robot import Lovebird
 
-myRobot = Lovebird()
+myRobot = Lovebird("Tessle")
 
 while 1:
     events = get_gamepad()
     left, right = 0, 0
     for event in events:
         if event.code == "ABS_Y":
-            left = (event.state - 127) * 2
+            left = event.state
             
         if event.code == "ABS_RZ":
-            right = (event.state - 127) * 2
+            right = event.state
     myrobot.drive(left, right) 
--- robot.py
+++ robot.py
@@ -5,25 +5,38 @@ import pyfirmata
 
 class Lovebird():
 
-    def __init__(self):
+    def __init__(self, robotName):
         board = pyfirmata.ArduinoMega('/dev/ttyACM0')
-        self.leftPower = board.get_pin("d:2:p")
-        self.rightPower = board.get_pin("d:22:p")
-        self.leftDir = board.get_pin("d:3:o")
-        self.rightDir = board.get_pin("d:23:o")
-    
-    def drive(leftMotor, rightMotor): #values between -255, 255
-        if leftMotor > 0:
-            leftDir.write(1)
-        else: 
-            leftDir.write(0)
+        self.name = robotName
+        if (robotName == "Lovebird")
+            self.leftPower = board.get_pin("d:2:p")
+            self.rightPower = board.get_pin("d:3:p")
+            self.leftDir = board.get_pin("d:22:o")
+            self.rightDir = board.get_pin("d:23:o")
+        elif (robotName == "Tessle"):
+            self.leftPower = board.get_pin("d:2:s")
+            self.rightPower = board.get_pin("d:3:p")
         
-        if rightMotor > 0:
-            rightMotor.write(1)
-        else: 
-            rightMotor.write(0)
+    def drive(self, leftMotor, rightMotor): #values between -255, 255
+        if self.name == "Lovebird":
+            left = (leftMotor - 127) * 2
+            right = (rightMotor - 127) * 2
+            if left > 0:
+                leftDir.write(1)
+            else: 
+                leftDir.write(0)
         
-        leftPower.write(abs(leftMotor))
-        rightPower.write(abs(rightMotor))
+            if right > 0:
+                rightMotor.write(1)
+            else: 
+                rightMotor.write(0)
+        
+            leftPower.write(abs(left))
+            rightPower.write(abs(right))
+
+        elif self.name == "Tessle":
+            leftPower.write(leftMotor)
+            rightMotor.write(rightMotor)
+        
         
     
