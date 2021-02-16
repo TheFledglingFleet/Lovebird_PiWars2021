1ae2d12e24b2f749cba92532f6272163f8c6eef4
The Fledgling Fleet <thefledglingfleet@gmail.com>
2021-02-16 07:14:40 -0600

    added encoder

--- controllerTJ.py
+++ controllerTJ.py
@@ -18,7 +18,7 @@ for bdaddr in nearby_devices:
 if target_address is not None:
     print("found target bluetooth device with address ", target_address)
     c = BluetoothClient(target_address, data_received)
-    c.send("a")
+    c.send("a".encode())
 
 else:
     print("could not find target bluetooth device nearby")
