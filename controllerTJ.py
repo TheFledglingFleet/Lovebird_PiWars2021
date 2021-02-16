ba2f8e72def1f792ed4ff8aa50ad5aa089927494
The Fledgling Fleet <thefledglingfleet@gmail.com>
2021-02-16 04:36:49 -0600

    testing bluedot

--- controllerTJ.py
+++ controllerTJ.py
@@ -1,10 +1,13 @@
 import bluetooth
+from bluedot.btcomm import BluetoothClient
 
 target_name = "HC-06"
 target_address = None
 port = 3
 
 nearby_devices = bluetooth.discover_devices()
+def data_received(data):
+    print(data)
 
 for bdaddr in nearby_devices:
     if target_name == bluetooth.lookup_name( bdaddr ):
@@ -13,12 +16,8 @@ for bdaddr in nearby_devices:
 
 if target_address is not None:
     print("found target bluetooth device with address ", target_address)
-    sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-    sock.connect((target_address, port))
-
-    sock.send("hello!!")
-
-    sock.close()
+    c = BluetoothClient("00:00:12:06:53:92", data_received)
+    c.send("ON")
 
 else:
     print("could not find target bluetooth device nearby")
