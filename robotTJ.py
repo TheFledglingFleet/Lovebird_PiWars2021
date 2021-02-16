ae4894cff66e43208267fe0905e1875745c692a9
The Fledgling Fleet <thefledglingfleet@gmail.com>
2021-02-16 17:09:22 -0600

    robotTJ: targetName updated

--- robotTJ.py
+++ robotTJ.py
@@ -14,11 +14,11 @@ class robot():
         nearby_devices = bluetooth.discover_devices()
         
         for bdaddr in nearby_devices:
-            if target_name == bluetooth.lookup_name( bdaddr ):
-                target_address = bdaddr
+            if self.target_name == bluetooth.lookup_name( bdaddr ):
+                self.target_address = bdaddr
                 break
-        if target_address is not None:
-            print("found target bluetooth device with address ", target_address)
+        if self.target_address is not None:
+            print("found target bluetooth device with address ", self.target_address)
             self.c = BluetoothClient(self.target_address, self.data_received)
             
     def run(self):
