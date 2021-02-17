5522c18bb81bb05ec98513028de5e54fc5e1ce22
The Fledgling Fleet <thefledglingfleet@gmail.com>
2021-02-16 18:23:58 -0600

    update sleep

--- robotTJ.py
+++ robotTJ.py
@@ -25,17 +25,17 @@ class robot():
     def run(self):
         left, right = 0, 0
         while(1):
-            time.sleep(0.015)
+            time.sleep(0.01)
             events = get_gamepad()
             
             maxSpeed = 60
             maxDivision = 255/maxSpeed
             for event in events:
                 if event.code == "ABS_Y":
-                    left = ((int(event.state) - 127) * 2)/maxDivision
+                    left = int((event.state - 127) * 2)/maxDivision
                 
                 if event.code == "ABS_RZ":
-                    right = ((int(event.state) - 127) * 2)/maxDivision
+                    right = int((event.state - 127) * 2)/maxDivision
             value = "<{0},{1}>".format(left, right)
             print(value)
             self.c.send(value)
